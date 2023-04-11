0c993bc9e1a73d67fd9e0210e46b6b5be99b3dc7
ohmindgod <LegendaryEnOt@bk.ru>
2023-04-11 20:44:02 +0400

    :sparkles: different display of components depending on the number of torrent files

--- pages/[type]/play/[magnet].vue
+++ pages/[type]/play/[magnet].vue
@@ -29,13 +29,20 @@ const Play = async (magnet: TorrentInfo['magnet']) => {
     isLoading.value = true
     try {
         const data = await addTorrent(magnet)
-        console.log(data)
-        data.forEach(item => {
-            video.value.push({
-                magnet: magnet,
-                fileName: item.name
+        if(data.length > 1){
+            data.forEach(item => {
+                video.value.push({
+                    magnet: magnet,
+                    fileName: item.name
+                })
             })
-        })
+        }
+        else{
+            currentVideo.value = {
+                magnet:magnet,
+                fileName:data[0].name
+            }
+        }
     } catch (er) {
         console.log('ERROR')
         isLoading.value = false
@@ -70,14 +77,14 @@ onUpdated(async () => {
             class="px-4 py-14 md:max-w-[1076px] xs:max-w-[614px] items-center justify-center flex gap-2 min-h-screen mx-auto my-0 relative">
             <!-- <div class="cover absolute -z-10 gradient"/> -->
             <div v-if="!isLoading && !error" class="page_body w-full h-full flex flex-col items-center justify-center">
-                <h4>
+                <h4 v-if="currentVideo">
                     {{ currentVideo?.fileName }}
                 </h4>
-                <div class="flex flex-col xs:flex-row ">
-                    <div class="media w-full xs:w-2/3">
+                <div class="flex flex-col xs:flex-row " :class="[{'items-center justify-center':video.length<1}]">
+                    <div class="media w-full transition-all" :class="[{'xs:w-2/3':video.length>1}]">
                         <PublicVideoPlayer :Source="apiURL.videoURL(videoUrl)" />
                     </div>
-                    <div class="xs:w-1/3">
+                    <div v-if="video.length>0" class="xs:w-1/3">
                         <ul>
                             <PublicTorrentListItem  v-for="torrent of video" :key="torrent.fileName" :item="torrent" @click.prevent="setCurrentVideo(torrent)"/>
                         </ul>
@@ -125,7 +132,5 @@ onUpdated(async () => {
 }
 
 /* This is just to transition when you change the viewport size. */
-* {
-  transition: all 0.25s ease-out;
-}
+
 </style>
