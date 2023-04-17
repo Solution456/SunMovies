e194467ec859469bc820f39f8e8f4f39ed2f00db
ohmindgod <LegendaryEnOt@bk.ru>
2023-04-17 18:03:25 +0400

    :hammer: test

--- server/api/tmdb/[...path].ts
+++ server/api/tmdb/[...path].ts
@@ -2,6 +2,7 @@ import {ofetch} from 'ofetch'
 import https, { Agent } from 'https';
 import HttpsProxyAgent from 'https-proxy-agent'
 
+const isDev = process.env.NODE_ENV === 'development'
 
 const httpsAgent = new https.Agent({
       rejectUnauthorized: false,
@@ -10,6 +11,8 @@ const httpsAgent = new https.Agent({
 
 const TMDB_API_URL = 'https://api.themoviedb.org/3'
 const TMDB_PROX = 'http://apitmdb.cub.watch/3/'
+
+const api = isDev? TMDB_PROX : TMDB_API_URL
 
 const TEST_API = 'https://official-joke-api.appspot.com'
 
